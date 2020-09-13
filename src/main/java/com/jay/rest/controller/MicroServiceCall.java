99c57c091d60637681433d40f334d06272609b21
Jay Patel <jpatel8110@gmail.com>
2020-09-13 17:14:24 -0400

    Making the port dynamic

--- src/main/java/com/jay/rest/controller/MicroServiceCall.java
+++ src/main/java/com/jay/rest/controller/MicroServiceCall.java
@@ -3,6 +3,7 @@ package com.jay.rest.controller;
 import com.jay.rest.models.Student;
 import com.jay.rest.models.StudentList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +15,14 @@ import java.util.List;
 @RequestMapping(value = "/micro")
 public class MicroServiceCall {
 
+    @Value("${Microservicetwo.port:Default 8086}")
+    private String port;
     @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping(value = "/",produces = "application/json")
     public StudentList callMicroServiceTwo() {
-       StudentList studentList = restTemplate.getForObject("http://localhost:8082/studentinfo/", StudentList.class);
+       StudentList studentList = restTemplate.getForObject("http://localhost:"+port+"/studentinfo/", StudentList.class);
         return studentList;
     }
 }
